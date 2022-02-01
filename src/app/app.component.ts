41145dadc1810e8e3d3c76df6838268b726926b3
kyuing <kyykyu000@gmail.com>
2022-02-01 14:08:18 +0000

    v1: initial commit, dev setup, and learning components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+//structure for every component
 import { Component } from '@angular/core';
 
+//decoration
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //html tag. embedded to <app-root></app-root> in index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash';
+  // title: string = 'Task Tracker';
+  // title = 'angular-crash';
 }
